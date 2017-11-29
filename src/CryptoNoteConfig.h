ec011a4c0c30ec3e70491877db96626d53b209c5
spiritaufe <34106375+spiritaufe@users.noreply.github.com>
2017-11-29 22:58:14 +0300

    Coin Name
    GezginCoin

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,3 +1,4 @@
+// Forked 2017.
 // Copyright (c) 2011-2016 The Cryptonote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -72,8 +73,7 @@ const char     CRYPTONOTE_BLOCKCHAIN_INDICES_FILENAME[]      = "blockchainindice
 const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
 } // parameters
 
-//TODO Put here the name of your currency
-const char     CRYPTONOTE_NAME[]                             = "";
+const char     CRYPTONOTE_NAME[]                             = "gezgincoin";
 const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
